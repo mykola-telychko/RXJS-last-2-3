72dc20ffcb52b0c408b5b1e0544ea8552c74b3f0
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-22 13:35:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-last-2-3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-e4nqcj)
---
+++ index.ts
@@ -0,0 +1,19 @@
+import { from } from 'rxjs';
+import { last } from 'rxjs/operators';
+
+const src1$ = from([1, 2, 3, 4, 5]);
+const src2$ = from([1, 2, 3, 4, 5]);
+
+// # 1
+//emit last even number
+const example1 = src1$.pipe(last((num) => num % 2 === 0));
+//output: "Last to pass test: 4"
+const subscribe1 = example1.subscribe((val) =>
+  console.log(`Last to pass test: ${val}`)
+);
+
+// # 2
+//no values will pass given predicate, emit default
+const example2 = src2$.pipe(last((v) => v > 5, 'else false!'));
+//output: 'Nothing!'
+const subscribe2 = example2.subscribe((val) => console.log(val));
